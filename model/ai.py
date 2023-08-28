06568f4470a53e248cf74be53ede825557b3ce12
Krishpy-Chips <101330162+Krishpy-Chips@users.noreply.github.com>
2023-08-28 18:49:57 +0400

    Finished training the first AI model

--- model/ai.py
+++ model/ai.py
@@ -3,52 +3,201 @@ import numpy
 import tensorflow
 import random
 import json
+import pickle
+import tflearn
 
 from nltk.stem.lancaster import LancasterStemmer
 from typing import Literal
 
-class Ai:
-	def __init__(self) -> None:
-		pass
 
-	def train_ai(self, dataset:str = "intents.json") -> None:
+class Ai:
+	def __init__(self, model_no:int , n_epochs:int, batch_size:int, dataset:str) -> None:
+		try:
+			nltk.data.find("tokenizers/punkt")
+		except LookupError:
+			nltk.download("punkt")		# This requires the device to be connected to the internet
+
+		self.MODEL_NO   = model_no
+		self.N_EPOCHS   = n_epochs   	# Number of times the model will train data from it
+		self.BATCH_SIZE = batch_size	# Number of middle neurons
+
+		self.stemmer = LancasterStemmer()
+		self.dataset = dataset		# The file location of the dataset over which the AI is going to train over
+
+		self.ignore_chars = ("?", "!", ".", ",", "|")
+
+		self.classify:	dict | None				= None
+		self.words:		list | None				= None
+		self.labels:	list | None				= None
+		self.training:	numpy.ndarray | None	= None
+		self.output:	numpy.ndarray | None	= None
+
+
+	def connect_model(self, training_: numpy.ndarray, output_: numpy.ndarray, mode: Literal["train", "load"]) -> (tflearn.DNN | None):
 		"""
-		@dataset: The fiel location of the dataset over which the AI is going to train over
+		Optimizes and creates / loads a fully connected neural network
 		"""
-		stemmer = LancasterStemmer()
+
+		tensorflow.compat.v1.reset_default_graph()
+
+		net = tflearn.input_data(shape=[None, len(training_[0])])
+		net = tflearn.regression(
+			tflearn.fully_connected(
+				tflearn.fully_connected(
+					tflearn.fully_connected(
+						net,
+					8),
+				8),
+			len(output_[0]), activation="softmax")	# Softmax gives its neurons probability range based on how accurate it is with it's output
+		)
+
+		model = tflearn.DNN(net)
+
+		if (mode := mode.lower().strip()) == "load":
+			model.load(rf"model\model_versions\MV{self.MODEL_NO}\generative_model_{self.MODEL_NO}.{self.N_EPOCHS}.{self.BATCH_SIZE}.tflearn")
+		
+		elif mode == "train":
+			model.fit(training_, output_, n_epoch=self.N_EPOCHS, batch_size=self.BATCH_SIZE, show_metric=True)
+			model.save(rf"model\model_versions\MV{self.MODEL_NO}\generative_model_{self.MODEL_NO}.{self.N_EPOCHS}.{self.BATCH_SIZE}.tflearn")
+		
+		else:
+			return None
+
+		return model
+
+	
+	def ask_model(self, user_input: str) -> str:
+		"""
+		@user_input: The question you want to ask the pre trained AI
+		"""
+
+		try:
+			with open(self.dataset, "r") as intents:
+				data = json.load(intents)
+
+		except FileNotFoundError:
+			return print("ERROR: The intents file doesn't exist for referencing model")
+		
+		if any(arr is None for arr in (self.words, self.labels, self.training, self.output)): #  Since self.training & self.output are an array, we need to manually check weather each elemnt is of None type. 
+			with open(rf"model\model_versions\MV{self.MODEL_NO}\train_data\training_data_{self.MODEL_NO}.bin", "rb") as training_data_file:
+				self.words, self.labels, self.training, self.output = pickle.load(training_data_file)
+			
+		if self.classify is None:
+			with open(rf"model\model_versions\MV{self.MODEL_NO}\train_data\intents_classify_{self.MODEL_NO}.bin", "rb") as intents_classify_file:
+				self.classify = pickle.load(intents_classify_file)
+		
+		model = self.connect_model(self.training, self.output, "load")
+
+		# Using the bag-of-words method over the user's input to search over the model's trained data
+		# Basically converting the user input to a format through which the AI was trained by
+		bag = [0 for _ in range(len(self.words))]
+
+		tokenized_words = [self.stemmer.stem(word.lower()) for word in nltk.word_tokenize(user_input)]
+
+		for t in tokenized_words:
+			for index, word in enumerate(self.words):
+				if word == t:
+					bag[index] = 1
+		
+		prediction_values = model.predict([numpy.array(bag)])
+		prediction_result =  numpy.argmax(prediction_values)
+		tag_result = self.labels[prediction_result]
+
+
+		#print(prediction_values)
+		#print(prediction_result)
+		#print(tag_result)
+		return random.choice(data["intents"][self.classify[tag_result]]["responses"])
+	
+
+	def train_model(self) -> None:
+		"""
+		To train the model
+		"""
 
 		words  = []
 		labels = []
 		docs_x = []
 		docs_y = []
 
+		# Parsing the dataset-
 		try:
-			with open(dataset, "r") as file:
-				data = json.load(file)
+			with open(self.dataset, "r") as intents:
+				data = json.load(intents)
 
 		except FileNotFoundError:
-			return print("The file doesn't exist")
+			return print("The intents file doesn't exist for training model")
 
 		for intent in data["intents"]:
 			for pattern in intent["patterns"]:
-
-				try:
-					tokenized_word = nltk.word_tokenize(pattern)
-				except LookupError:
-					nltk.download("punkt")
-					tokenized_word = nltk.word_tokenize(pattern)
-					
+				tokenized_word = nltk.word_tokenize(pattern)	# Getting the root word
 				words.extend(tokenized_word)
-				docs_x.append(pattern)
+				docs_x.append(tokenized_word )
 				docs_y.append(intent["tag"])
 			
 			if intent["tag"] not in labels:
 				labels.append(intent["tag"])
 			
-		words = [stemmer.stem(w.lower()) for w in words] 	# The AI's vocabulary
+		words = [self.stemmer.stem(w.lower()) for w in words if w not in self.ignore_chars] 	# The AI's vocabulary
 		words = sorted(list(set(words)))	# Formatting; removing duplicates & rearranging words
 		labels = sorted(labels)
 
-		print(words)
+		# Using bag-of-words method to convert it's vocabulary to a vector word count.
+		training = []
+		output   = []
 
-Ai().train_ai()
+		out_empty = [0 for _ in range(len(labels))]
+
+		for x, doc in enumerate(docs_x):
+			bag = []
+
+			stemmed_words = [self.stemmer.stem(w.lower()) for w in doc]
+
+			for w in words:
+				if w in stemmed_words:
+					bag.append(1)
+					continue
+
+				bag.append(0)
+		
+			output_row = out_empty.copy()
+			output_row[labels.index(docs_y[x])] = 1
+
+			training.append(bag)
+			output.append(output_row)
+
+		training = numpy.array(training)
+		output   = numpy.array(output)
+
+		with open(rf"model\model_versions\MV{self.MODEL_NO}\train_data\training_data_{self.MODEL_NO}.bin", "wb") as training_data_file:
+			pickle.dump((words, labels, training, output), training_data_file)
+		
+		with open(rf"model\model_versions\MV{self.MODEL_NO}\train_data\intents_classify_{self.MODEL_NO}.bin", "wb") as intents_classify_file:
+
+			classify = {}
+
+			for index, content in enumerate(data["intents"]):
+				classify[content["tag"]] = index
+
+			pickle.dump(classify, intents_classify_file)
+
+		self.connect_model(training, output, "train")
+
+		# Training over the data-
+		#net = tflearn.input_data(shape=[None, len(training[0])])
+		#net = tflearn.regression(
+		#	tflearn.fully_connected(
+		#		tflearn.fully_connected(
+		#			tflearn.fully_connected(
+		#				net,
+		#			8),
+		#		8),
+		#	len(output[0]), activation="softmax")	# Softmax gives its neurons probability range based on how accurate it is with it's output
+		#)
+#
+		#model = tflearn.DNN(net)
+		#model.fit(training, output, n_epoch=self.N_EPOCHS, batch_size=self.BATCH_SIZE, show_metric=True)
+		#model.save(rf"model\model_versions\MV{self.MODEL_NO}\generative_model_{self.MODEL_NO}.{self.N_EPOCHS}.{self.BATCH_SIZE}.tflearn")
+
+
+#Ai(0, 500, 8, "intents.json").train_model()
